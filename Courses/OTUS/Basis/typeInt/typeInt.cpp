23ea7ba9d0ccdd66aeb8766772edf65633798f44
Denis Frolov <d.fr8938@gmail.com>
2020-07-07 11:58:44 +0300

    modify file typeInt and .gitignore

--- Courses/OTUS/Basis/typeInt/typeInt.cpp
+++ Courses/OTUS/Basis/typeInt/typeInt.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* argv[]) {
     std::cout << "sizeof(ui) = " << +sizeof(ui) << std::endl;
     std::cout << "unsigned int min = " << +std::numeric_limits<unsigned int>::min() << std::endl;
     std::cout << "unsigned int max = " << +std::numeric_limits<unsigned int>::max() << std::endl;
+    std::cout << std::endl;
 
     signed int si = 32000;
 
@@ -18,6 +19,7 @@ int main(int argc, char* argv[]) {
     std::cout << "sizeof(si) = " << +sizeof(si) << std::endl;
     std::cout << "signed int min = " << +std::numeric_limits<signed int>::min() << std::endl;
     std::cout << "signed int max = " << +std::numeric_limits<signed int>::max() << std::endl;
+    std::cout << std::endl;
 
     int i = 32000;
 
@@ -25,6 +27,7 @@ int main(int argc, char* argv[]) {
     std::cout << "sizeof(ui) = " << sizeof(i) << std::endl;
     std::cout << "int min = " << +std::numeric_limits<int>::min() << std::endl;
     std::cout << "int max = " << +std::numeric_limits<int>::max() << std::endl;
+    std::cout << std::endl;
 
     return 0;
 
